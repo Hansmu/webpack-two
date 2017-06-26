3b6734228657c58986bb8c7bcf67511efe7fe092
Hans Miikael Uuspõld <hans.miikael.uuspold@icefire.ee>
2017-06-26 16:18:23 +0300

    Linked modules example with webpack. Basic webpack example.

---
+++ js_modules/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path'); // path comes from NodeJS. Helper to give absolute path.
+
+const config = {
+    entry: './src/index.js', // Relative path.
+    output: {
+        path: path.resolve(__dirname, 'build'), // Path to save to. Requires absolute path. Node makes sure we get the correct path with path.resolve.
+        //  __dirname is a constant in NodeJS. A reference to the current directory.
+        filename: 'bundle.js' // Whatever name you want. Generally called bundle.js
+    }
+};
+
+module.exports = config;
