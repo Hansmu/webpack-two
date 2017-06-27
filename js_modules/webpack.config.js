c9d05158a7a0ee2f7f95a900cd187503509f216f
Hansmu <hans.miikael@gmail.com>
2017-06-27 15:32:16 +0300

    Loader example (Babel) in Webpack

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -6,6 +6,14 @@ const config = {
         path: path.resolve(__dirname, 'build'), // Path to save to. Requires absolute path. Node makes sure we get the correct path with path.resolve.
         //  __dirname is a constant in NodeJS. A reference to the current directory.
         filename: 'bundle.js' // Whatever name you want. Generally called bundle.js
+    },
+    module: { //    Module system
+        rules: [    //  Rules are loaders.
+            {
+                use: 'babel-loader', // Say which loader to run.
+                test: /\.js$/,  //  Regular expression. Taken by Webpack and applied to every file. Checks if file ends with .js, then Babel will be applied.
+            }
+        ]
     }
 };
 
