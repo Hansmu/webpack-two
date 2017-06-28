1adcf8ebf078ac1b2e174e490a4b619bfb0bdcd8
Hans Miikael Uuspõld <hans.miikael.uuspold@icefire.ee>
2017-06-28 11:06:31 +0300

    Codesplitting

--- js_modules/src/image_viewer.js
+++ js_modules/src/image_viewer.js
@@ -1,13 +1,9 @@
 import '../styles/image_viewer.css';
-import big from '../assets/big.jpg';    //  URL to file
 import small from '../assets/small.jpg';    //  String of the image in base64
 
-const image = document.createElement('img');
-image.src = small;
+export default () => {
+    const image = document.createElement('img');
+    image.src = small;
 
-document.body.appendChild(image);
-
-const bigImage = document.createElement('img');
-bigImage.src = big;
-
-document.body.appendChild(bigImage);
+    document.body.appendChild(image);
+}
--- js_modules/src/index.js
+++ js_modules/src/index.js
@@ -1,5 +1,14 @@
-import sum from './sum';
-import './image_viewer';    //  Doesn't actually import any executable code. All it has to do is run in our module. That's why it's a different import.
+const button = document.createElement('button');
+button.innerText = 'Click Me!';
+button.onclick = () => {
+    //  System is a global variable inside of JS. System.import is part of the ES2015 spec.
+    //  We pass in the name of the module we want to import. Then our application will reach out to the server and ask for the module.
+    //  Server finds it, returns it to the client and then we can execute it.
+    //  We only pull in the image_viewer module, but any import that it has inside it we pull in as well with the module.
+    //  This is asynchronous code, so it returns a promise.
+    System.import('./image_viewer').then(module => {
+        module.default();
+    });
+};
 
-const total = sum(10, 5);
-console.log(total);
+document.body.appendChild(button);
