c4e855ad58c962273a3c4fbe417691914f3fe975
Hans Miikael Uuspõld <hans.miikael.uuspold@icefire.ee>
2017-06-28 13:51:41 +0300

    Code splitting for vendor and our code. Plugin to automatically inject all our JS file references to our HTML template.

--- large_project/webpack.config.js
+++ large_project/webpack.config.js
@@ -1,11 +1,20 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+    'faker', 'lodash', 'redux', 'react-redux', 'react-dom',
+    'react', 'react-input-range', 'redux-form', 'redux-thunk'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+      bundle: './src/index.js',  //  I wanted to produce a bundle.js file as the property is called bundle.
+      vendor: VENDOR_LIBS   //  I want a different bundle. Provide an array with the different vendor files. Put all the regular dependencies in there.
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'   //  Uses the property defined above to create a file. When bundle file has been created, save it as bundle.js. When vendor has been created, save it as vendor.js.
   },
   module: {
     rules: [
@@ -19,5 +28,13 @@ module.exports = {
             test: /\.css$/
         }
     ]
-  }
+  },
+  plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+          name: 'vendor'    //  Looks at our entry points and if it finds common/duplicate code, then pulls it out and puts it in the vendor file that's created in entry.
+      }),
+      new HtmlWebpackPlugin({
+          template: 'src/index.html'
+      })
+  ]
 };
